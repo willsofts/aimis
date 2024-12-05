3d52761a677dc2d928ea6e3d026c914b281061d5
tassun <32536248+tassun@users.noreply.github.com>
2024-12-05 10:53:57 +0700

    update docker info

--- docker_push.bat
+++ docker_push.bat
@@ -2,7 +2,7 @@ set REGISTRY_USER=willsofts
 set REGISTRY_PASSWORD=Tassan@2024
 set IMAGE_NAME=willsofts/aimis
 set APP_NAME=willsofts/aimis
-set APP_VERSION=1.0.0
+set APP_VERSION=1.0.1
 docker login --username %REGISTRY_USER% --password %REGISTRY_PASSWORD%
 docker tag %IMAGE_NAME% %APP_NAME%:%APP_VERSION%
 docker tag %IMAGE_NAME% %APP_NAME%:latest
