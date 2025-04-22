2c934eec13b7771f09610d98c927869384ea7fda
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-04-22 08:56:01 +0700

    support transporter

---
+++ config/moleculer.config.redis.js
@@ -0,0 +1,58 @@
+var os = require("os");
+
+module.exports = {
+    nodeID: "aimis-"+os.hostname().toLowerCase() + "-" + process.pid,
+    logger: [
+        {
+            type: "Console",
+            options: {
+                level: "debug",
+                color: true,
+                formatter: "full",
+            } 
+        },                   
+        /*
+        {
+            type: "File",
+            options: {
+                level: "debug",
+                formatter: "full",
+                folder: "./logs",
+                filename: "express-{date}.log",
+                eol: "\n",
+                interval: 1000,
+            }
+        }*/
+    ],
+    registry: {
+        strategy: "RoundRobin"
+    },
+    transporter: "redis://redisstack:6379",
+    /*    
+    transporter: "NATS",
+    transporter: "nats://localhost:4222",
+    requestTimeout: 5 * 1000,
+
+    circuitBreaker: {
+        enabled: true
+    },
+
+    metrics: false,
+    statistics: true
+    */
+   /*
+    tracing: {
+        enabled: true,
+        exporter: {
+            type: "Jaeger",
+            options: {
+                endpoint: null, //"http://localhost:14250", //not work               
+                host: "localhost",
+                port: 6832,
+                tracerOptions: {},
+                defaultTags: [{"module.name":"mext"}]
+            }
+        }
+    },   
+    */
+};
