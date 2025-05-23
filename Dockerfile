b5dfb111a05d0635fffded3b47a58cf6ed53e096
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-05-23 12:38:57 +0700

    using token from command line

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:20.18.0
 
+ARG NPM_TOKEN
 ENV NODE_ENV=production
-ENV NPM_TOKEN=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
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
@@ -9,7 +9,7 @@ WORKDIR /usr/src/app
 COPY package.json /usr/src/app/
 COPY .npmrc /usr/src/app/
 
-RUN npm install --production --omit=dev
+RUN npm install --omit=dev
 
 COPY . /usr/src/app
 
--- docker_build.bat
+++ docker_build.bat
@@ -1,6 +1,7 @@
 echo "copy default_aiven.json to default.json"
 copy /Y .\config\default-aiven.json .\config\default.json
 echo "start build docker image"
-docker build -t willsofts/aimis .
+docker build --build-arg NPM_TOKEN=%NPM_TOKEN% -t willsofts/aimis .
+rem docker build -t willsofts/aimis .
 echo "copy default_localhost.json to default.json"
 copy /Y .\config\default-localhost.json .\config\default.json
